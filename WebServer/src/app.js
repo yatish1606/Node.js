dff15e753e6f0b7377a88481215e740435ac1514
Yatish Kelkar <yatish1606@gmail.com>
2020-05-28 21:38:40 +0530

    render 404 pages

--- WebServer/src/app.js
+++ WebServer/src/app.js
@@ -1,32 +1,42 @@
 const express = require('express')
 const path = require('path')
+const hbs = require('hbs')
 
 const app = express()
 app.set('view engine', 'hbs')
+
+const partialsPath = path.join(__dirname,'../src/partials')
+
 app.use(express.static(path.join(__dirname, '../public')))
+hbs.registerPartials(partialsPath)
 
 app.get('/', (req,res) => {
     res.render('index', {
-        title : 'thisistitel'
+        title : 'Homepage',
+        footerText : 'Created by me'
     })
 })
 
 app.get('/about', (req,res) => {
     res.render('about', {
-        title:'Hey',     
+        title:'About',
+        footerText : 'Created by me'  
     })
 })
 
 app.get('/help', (req,res) => {
     res.render('help', {
-        title:'odnosdfunosuefnodcn',     
+        title:'Help', 
+        footerText : 'Created by me'    
     })
 })
 
 app.get('/weather', (req,res) => {
     res.send({
         forecast : 'Sunny',
-        location : 'Philadelphia'
+        location : 'Philadelphia',
+        title: 'weather',
+        footerText : 'Created by me'
     })
 })
 
