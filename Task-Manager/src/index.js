87321404b9d94d13f2d4800b8b54ba56a2b835af
Yatish Kelkar <yatish1606@gmail.com>
2020-06-03 10:18:40 +0530

    Reformat with asynchronous functions

--- Task-Manager/src/index.js
+++ Task-Manager/src/index.js
@@ -8,22 +8,26 @@ const port = process.env.PORT || 3000
 
 app.use(express.json())
 
-app.post('/users', (req, res) => {
+app.post('/users', async (req, res) => {
     const user = new User(req.body)
-
-    user.save().then(() => {
+    try {
+        await user.save()
         res.status(201).send(user)
-    }).catch((e) => {
+    } catch (e) {
         res.status(400).send(e)
-    })
+    }
+    
 })
 
-app.post('/tasks', (req,res) => {
+app.post('/tasks', async (req,res) => {
     const newTask = new Task(req.body)
 
-    newTask.save()
-        .then(()=>res.status(201).send(newTask))
-        .catch((e) => {res.status(400).send(e)})
+    try {
+        await newTask.save()
+        res.status(201).send(newTask)
+    } catch (e) {
+        res.status(400).send(e)
+    }
 })
 
 app.patch('/users/:id', (req,res) => {
@@ -38,41 +42,64 @@ app.patch('/tasks/:taskID', (req,res) => {
         .catch(e => res.status(400).send(e))
 })
 
-app.get('/users', (req,res) => {
-    User.find({})
-        .then( users => res.status(200).send(users))
-        .catch(e => res.status(500).send(e))
+app.get('/users', async (req,res) => {
+
+    try {
+        const users = await User.find({})
+    
+        if(!users) {
+            return res.status(404).send()
+        }
+
+        res.status(201).send(users)
+    } catch(e) {
+        res.status(500).send(e)
+    }
+    
 })
 
-app.get('/users/:id', (req,res) => {
+app.get('/users/:id', async (req,res) => {
 
     const _id = req.params.id
-    User.findById(_id)
-        .then(user => {
-            if(!user) {
-                res.status(404).send()
-            }
-            res.send(user)
-        })
-        .catch(e => res.status(500).send())
+    
+    try {
+        const foundUser = await User.findById(_id)
+        if(!foundUser){
+            return res.status(404).send()
+        }
+        res.send(foundUser)
+    } catch(e) {
+        res.status(500).send()
+    }
+    
 })
 
-app.get('/tasks', (req,res) => {
-    Task.find({})
-        .then( tasks => res.status(200).send(tasks))
-        .catch(e => res.status(500).send(e))
+app.get('/tasks', async (req,res) => {
+    
+    try {
+        const tasks = await Task.find({})
+
+        if(!tasks){
+            return res.status(404).send()
+        }
+        res.send(tasks)
+    } catch (e) {
+        res.status(500).send(e)
+    }
 })
 
-app.get('/tasks/:taskID', (req,res) => {
-    Task.findById(req.params.taskID)
-        .then( task => {
-            if(!task) {
-                res.status(404).send()
-            }
+app.get('/tasks/:taskID', async (req,res) => {
+    const _id = req.params.taskID
+    try {
+        const foundTask = await Task.findById(_id)
 
-            res.send(task)
-        })
-        .catch(e => res.status(500).send())
+        if(!foundTask){
+            return res.status(404).send()
+        }
+        res.send(foundTask)
+    } catch(e) {
+        res.status(500).send()
+    }
 })
 
 app.listen(port, () => {
