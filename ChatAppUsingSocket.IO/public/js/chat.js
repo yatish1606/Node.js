4ab30495157ad9af886e0dfbbfffe1e045786927
Yatish Kelkar <yatish1606@gmail.com>
2020-06-17 10:55:15 +0530

    Add UI

--- ChatAppUsingSocket.IO/public/js/chat.js
+++ ChatAppUsingSocket.IO/public/js/chat.js
@@ -1,11 +1,34 @@
+// CLIENT SIDE CODE
+
 const socket = io()
 
 socket.on('message', (message) => {
     console.log(message)
 })
 
+
 document.querySelector('#submit-form').addEventListener('submit', (e) => {
     e.preventDefault()
     const message = e.target.elements.message.value
-    socket.emit('sendMessage', message)
+    socket.emit('sendMessage', message , (error) => {
+        if(error) {
+            return console.log(error)
+        }
+        console.log('Message delivered')
+    })
+})
+
+document.querySelector('#sendLocationButton').addEventListener('click', () => {
+    if(!navigator.geolocation) {
+        return alert('Geolocation is not supported by your browser')
+    }
+
+    navigator.geolocation.getCurrentPosition(position => {
+        socket.emit('sendLocation', {
+            latitude : position.coords.latitude,
+            longitude : position.coords.longitude
+        }, () => {
+            console.log('Location shared')
+        })
+    })
 })
--- ChatAppUsingSocket.IO/src/index.js
+++ ChatAppUsingSocket.IO/src/index.js
@@ -1,7 +1,10 @@
+// SERVER SIDE CODE
+
 const path = require('path')
 const express = require('express')
 const http = require('http')
 const socketIO = require('socket.io')
+const Filter = require('bad-words')
 
 const app = express()
 const server = http.createServer(app)
@@ -14,14 +17,49 @@ app.use(express.static(publicDirectoryPath))
 
 let count = 0
 
+// all events associated with socket.function() are server => client
+// all events associated with io.function() are client => server 
+// this is reversed in chat.js as const socket = io()
+
 io.on('connection', (socket) => {
+
+    // this code will run when a new client is connected
+    // clients.forEach(client => run io.on('connection' , ()=> {} ))
+
     console.log('New socket connection')
 
-    socket.emit('message', 'Welcome')
+    // this will send a welcome message to a new user who has joined the chat
+    socket.emit('message', 'Welcome') 
+
+    // socket.emit() => emit a message only to that connection
+    // socket.broadcat.emit() => emit a message to all except new connection
+
+    // send a message to all users except the new user
+    socket.broadcast.emit('message', "A new user just joined the chat")
     
-    socket.on('sendMessage', (message) => {
+    socket.on('sendMessage', (message, callback) => {
+
+        const filter = new Filter()
+
+        if(filter.isProfane(message)) {
+            return callback('Profanity is not allowed!')
+        }
+
         io.emit('message', message)
+        callback()
     })
+
+    // when any user shares their location, emit that message to all users
+    socket.on('sendLocation', (locationObject, callback) => {
+        io.emit('message', `https://google.com/maps?q=${locationObject.latitude},${locationObject.longitude}`)
+        callback()
+    })
+
+    // socket.on('disconnect') will run when any user leaves the chat or connection is broken
+    socket.on('disconnect', () => {
+        io.emit('message', "A user has left the chat")
+    })
+
 })
 
 
